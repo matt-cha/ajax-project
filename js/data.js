1e4505c9bb82661e63024c5a533548604c542c02
Matt Cha <mcha002@ucr.edu>
2023-01-17 21:41:38 +0000

    event listener to handle the 'pagehide'

--- js/data.js
+++ js/data.js
@@ -10,8 +10,9 @@ window.addEventListener('beforeunload', jsonStringify);
 function jsonStringify(event) {
   var formJsonString = JSON.stringify(data);
   localStorage.setItem('form-values', formJsonString);
-
 }
+
+window.addEventListener('pagehide', jsonStringify);
 
 var formJson = localStorage.getItem('form-values');
 if (formJson !== null) {
