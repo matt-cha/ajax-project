c1ee005af02e85516a4b6731438a7a945c8f5c73
Matt Cha <mcha002@ucr.edu>
2023-01-18 22:13:45 +0000

    Added Pre seed data 9 villagers

--- js/data.js
+++ js/data.js
@@ -2,8 +2,187 @@
 
 var data = {
   individualVillagerCard: null,
-  entries: [],
-  liked: []
+  entries: [{
+    birthdayString: 'June 26th',
+    hobby: 'Music',
+    icon: 'https://acnhapi.com/v1/icons/villagers/268',
+    id: 268,
+    image: 'https://acnhapi.com/v1/images/villagers/268',
+    name: 'Marina',
+    personality: 'Normal',
+    saying: "If you can't say something nice, don't say anything at all."
+  },
+  {
+    birthdayString: 'January 4th',
+    hobby: 'Education',
+    icon: 'https://acnhapi.com/v1/icons/villagers/109',
+    id: 109,
+    image: 'https://acnhapi.com/v1/images/villagers/109',
+    name: 'Diana',
+    personality: 'Snooty',
+    saying: 'Sometimes you have to lose to win.'
+  },
+  {
+    birthdayString: 'November 4th',
+    hobby: 'Play',
+    icon: 'https://acnhapi.com/v1/icons/villagers/113',
+    id: 113,
+    image: 'https://acnhapi.com/v1/images/villagers/113',
+    name: 'Lucky',
+    personality: 'Lazy',
+    saying: "Don't bite the hand that feeds you."
+  },
+  {
+    birthdayString: 'March 7th',
+    hobby: 'Nature',
+    icon: 'https://acnhapi.com/v1/icons/villagers/142',
+    id: 142,
+    image: 'https://acnhapi.com/v1/images/villagers/142',
+    name: 'Molly',
+    personality: 'Normal',
+    saying: "Like water off a duck's back."
+  },
+  {
+    birthdayString: 'March 10th',
+    hobby: 'Music',
+    icon: 'https://acnhapi.com/v1/icons/villagers/80',
+    id: 80,
+    image: 'https://acnhapi.com/v1/images/villagers/80',
+    name: 'Judy',
+    personality: 'Snooty',
+    saying: "I'm dancing my dreams away."
+  },
+  {
+    birthdayString: 'April 5th',
+    hobby: 'Nature',
+    icon: 'https://acnhapi.com/v1/icons/villagers/108',
+    id: 108,
+    image: 'https://acnhapi.com/v1/images/villagers/108',
+    name: 'Beau',
+    personality: 'Lazy',
+    saying: 'You snooze, you lose.'
+  },
+  {
+    birthdayString: 'January 18th',
+    hobby: 'Nature',
+    icon: 'https://acnhapi.com/v1/icons/villagers/180',
+    id: 180,
+    image: 'https://acnhapi.com/v1/images/villagers/180',
+    name: 'Sherb',
+    personality: 'Lazy',
+    saying: 'Forty winks is never enough.'
+  },
+  {
+    birthdayString: 'September 29th',
+    hobby: 'Music',
+    icon: 'https://acnhapi.com/v1/icons/villagers/372',
+    id: 372,
+    image: 'https://acnhapi.com/v1/images/villagers/372',
+    name: 'Marshal',
+    personality: 'Smug',
+    saying: 'Seize the day.'
+  },
+  {
+    birthdayString: 'October 1st',
+    hobby: 'Nature',
+    icon: 'https://acnhapi.com/v1/icons/villagers/64',
+    id: 64,
+    image: 'https://acnhapi.com/v1/images/villagers/64',
+    name: 'Raymond',
+    personality: 'Smug',
+    saying: 'Stay on brand!'
+  }],
+
+  liked: [{
+    birthdayString: 'June 26th',
+    hobby: 'Music',
+    icon: 'https://acnhapi.com/v1/icons/villagers/268',
+    id: 268,
+    image: 'https://acnhapi.com/v1/images/villagers/268',
+    name: 'Marina',
+    personality: 'Normal',
+    saying: "If you can't say something nice, don't say anything at all."
+  },
+  {
+    birthdayString: 'January 4th',
+    hobby: 'Education',
+    icon: 'https://acnhapi.com/v1/icons/villagers/109',
+    id: 109,
+    image: 'https://acnhapi.com/v1/images/villagers/109',
+    name: 'Diana',
+    personality: 'Snooty',
+    saying: 'Sometimes you have to lose to win.'
+  },
+  {
+    birthdayString: 'November 4th',
+    hobby: 'Play',
+    icon: 'https://acnhapi.com/v1/icons/villagers/113',
+    id: 113,
+    image: 'https://acnhapi.com/v1/images/villagers/113',
+    name: 'Lucky',
+    personality: 'Lazy',
+    saying: "Don't bite the hand that feeds you."
+  },
+  {
+    birthdayString: 'March 7th',
+    hobby: 'Nature',
+    icon: 'https://acnhapi.com/v1/icons/villagers/142',
+    id: 142,
+    image: 'https://acnhapi.com/v1/images/villagers/142',
+    name: 'Molly',
+    personality: 'Normal',
+    saying: "Like water off a duck's back."
+  },
+  {
+    birthdayString: 'March 10th',
+    hobby: 'Music',
+    icon: 'https://acnhapi.com/v1/icons/villagers/80',
+    id: 80,
+    image: 'https://acnhapi.com/v1/images/villagers/80',
+    name: 'Judy',
+    personality: 'Snooty',
+    saying: "I'm dancing my dreams away."
+  },
+  {
+    birthdayString: 'April 5th',
+    hobby: 'Nature',
+    icon: 'https://acnhapi.com/v1/icons/villagers/108',
+    id: 108,
+    image: 'https://acnhapi.com/v1/images/villagers/108',
+    name: 'Beau',
+    personality: 'Lazy',
+    saying: 'You snooze, you lose.'
+  },
+  {
+    birthdayString: 'January 18th',
+    hobby: 'Nature',
+    icon: 'https://acnhapi.com/v1/icons/villagers/180',
+    id: 180,
+    image: 'https://acnhapi.com/v1/images/villagers/180',
+    name: 'Sherb',
+    personality: 'Lazy',
+    saying: 'Forty winks is never enough.'
+  },
+  {
+    birthdayString: 'September 29th',
+    hobby: 'Music',
+    icon: 'https://acnhapi.com/v1/icons/villagers/372',
+    id: 372,
+    image: 'https://acnhapi.com/v1/images/villagers/372',
+    name: 'Marshal',
+    personality: 'Smug',
+    saying: 'Seize the day.'
+  },
+  {
+    birthdayString: 'October 1st',
+    hobby: 'Nature',
+    icon: 'https://acnhapi.com/v1/icons/villagers/64',
+    id: 64,
+    image: 'https://acnhapi.com/v1/images/villagers/64',
+    name: 'Raymond',
+    personality: 'Smug',
+    saying: 'Stay on brand!'
+  }]
 };
 
 window.addEventListener('beforeunload', jsonStringify);
@@ -18,3 +197,23 @@ var formJson = localStorage.getItem('form-values');
 if (formJson !== null) {
   data = JSON.parse(formJson);
 }
+
+/* [{
+birthdayString:"June 26th",hobby: "Music",icon:  "https://acnhapi.com/v1/icons/villagers/268",id:  268, image:  "https://acnhapi.com/v1/images/villagers/268",
+name:  "Marina",personality: "Normal",saying: "If you can't say something nice, don't say anything at all."},
+{birthdayString:  "January 4th",hobby:  "Education",icon:  "https://acnhapi.com/v1/icons/villagers/109",id:  109, image:  "https://acnhapi.com/v1/images/villagers/109",
+name:  "Diana",personality:  "Snooty",saying:  "Sometimes you have to lose to win."},
+{ birthdayString:  "November 4th",hobby:  "Play",icon:  "https://acnhapi.com/v1/icons/villagers/113",id:  113, image:  "https://acnhapi.com/v1/images/villagers/113",
+name:  "Lucky",personality:  "Lazy",saying:  "Don't bite the hand that feeds you."},
+{birthdayString:  "March 7th",hobby:  "Nature",icon:  "https://acnhapi.com/v1/icons/villagers/142",id:  142, image:  "https://acnhapi.com/v1/images/villagers/142",
+name:  "Molly",personality:  "Normal",saying:  "Like water off a duck's back."},
+{birthdayString:  "March 10th",hobby:  "Music",icon:  "https://acnhapi.com/v1/icons/villagers/80",id:  80, image:  "https://acnhapi.com/v1/images/villagers/80",
+name:  "Judy",personality:  "Snooty",saying:  "I'm dancing my dreams away."},
+{birthdayString:  "April 5th",hobby:  "Nature",icon:  "https://acnhapi.com/v1/icons/villagers/108",id:  108, image:  "https://acnhapi.com/v1/images/villagers/108",
+name:  "Beau",personality:  "Lazy",saying:  "You snooze, you lose."},
+{birthdayString:  "January 18th",hobby:  "Nature",icon:  "https://acnhapi.com/v1/icons/villagers/180",id:  180, image:  "https://acnhapi.com/v1/images/villagers/180",
+name:  "Sherb",personality:  "Lazy",saying:  "Forty winks is never enough."},
+{birthdayString:"September 29th",hobby:  "Music",icon:  "https://acnhapi.com/v1/icons/villagers/372",id:  372, image:  "https://acnhapi.com/v1/images/villagers/372",
+name:  "Marshal",personality:  "Smug",saying:  "Seize the day."},
+{birthdayString:  "October 1st",hobby:  "Nature",icon:  "https://acnhapi.com/v1/icons/villagers/64",id:  64, image:  "https://acnhapi.com/v1/images/villagers/64",
+name:  "Raymond",personality:  "Smug",saying:  "Stay on brand!"}] */
